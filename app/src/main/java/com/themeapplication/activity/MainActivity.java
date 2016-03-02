ab1b601ee986c1336ccac3be794094dde3a61903
ajayarya4289 <dragon@4289>
2016-03-03 00:40:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/themeapplication/activity/MainActivity.java
@@ -0,0 +1,88 @@
+package com.themeapplication.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.themeapplication.R;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    private Button mRedBtn;
+
+    private Button mBlueBtn;
+
+    private Button mDoneButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mRedBtn = (Button) findViewById(R.id.red_btn);
+        mRedBtn.setOnClickListener(this);
+
+        mBlueBtn = (Button) findViewById(R.id.blue_btn);
+        mBlueBtn.setOnClickListener(this);
+
+        mDoneButton = (Button) findViewById(R.id.done_btn);
+        mDoneButton.setOnClickListener(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.red_btn:
+
+                break;
+            case R.id.blue_btn:
+
+                break;
+            case R.id.done_btn:
+
+                break;
+            default:
+
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/themeapplication/utils/ThemeUtils.java
@@ -0,0 +1,19 @@
+package com.themeapplication.utils;
+
+/**
+ * Created by ajay on 2/3/16.
+ */
+public class ThemeUtils {
+
+    public int selectedTheme = 0;
+
+    public static enum THEME {
+        DEFAULT,
+        RED,
+        BLUE
+    };
+
+
+
+
+}
